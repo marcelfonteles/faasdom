d9a6716b19b21cb87fddb0703f1e32b9d732efa5
marcelfonteles <marcelfonteles@gmail.com>
2022-08-29 20:40:03 -0300

    rust azure matrix test

--- azure/src/rust/rust_matrix/src/main.rs
+++ azure/src/rust/rust_matrix/src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
+use rand::Rng;
 use std::env;
 use std::net::Ipv4Addr;
 use warp::{Filter};
-use serde_json::{Value, Map, json};
+use serde_json::{Value, Map};
 
 
 #[tokio::main]
@@ -14,21 +15,49 @@ async fn main() {
         return number_int;
     }
 
-    fn factors(n: i64) -> Vec<i64> {
-        let mut vec = Vec::with_capacity(100);
-        let mut i: i64 = 1;
-        while i <= f64::sqrt(n as f64) as i64 {
-            if n % i == 0 {
-                vec.push(i);
-                if n / i != i {
-                    vec.push(n / i);
-                }    
+    fn random_table(n: i64) -> Vec<Vec<i64>> {
+        let mut vec1 = Vec::with_capacity(n.try_into().unwrap());
+        let mut rng = rand::thread_rng();
+
+        let mut i: i64 = 0;
+        while i < n {
+            vec1.push(Vec::with_capacity(n.try_into().unwrap()));
+            
+            let mut j: i64 = 0;
+            while j < n {
+                vec1[i as usize].push(rng.gen_range(0..100));
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+        
+        return vec1;
+    }
+
+    fn matrix(n: i64) -> Vec<Vec<i64>> {
+        let matrix_a = random_table(n);
+        let matrix_b = random_table(n);
+        let mut matrix_mult = Vec::with_capacity(n.try_into().unwrap());
+
+        let mut i: usize = 0;
+        while i < matrix_a.len() {
+            matrix_mult.push(Vec::with_capacity(n.try_into().unwrap()));
+
+            let mut j: usize = 0;
+            while j < matrix_b.len() {
+                let mut sum = 0;
+                let mut k: usize = 0;
+                while k  < matrix_a.len() {
+                    sum = sum + matrix_a[i][k] * matrix_b[k][j];
+                    k = k + 1;
+                }
+                matrix_mult[i as usize].push(sum);
+                j = j + 1;
             }
             i = i + 1;
         }
 
-        vec.sort();
-        return vec;
+        return matrix_mult;
     }
 
     let example1 = warp::get()
@@ -38,29 +67,25 @@ async fn main() {
         .map(|p: HashMap<String, String>| match p.get("n") {
             Some(n) => {
                 let integer_number = convert(n);
-                let res_factors = factors(integer_number);
+                matrix(integer_number);
 
                 let mut map = Map::new();
                 let mut inner_map = Map::new();
-                inner_map.insert("test".to_string(), serde_json::Value::String("cpu test".to_string()));
+                inner_map.insert("test".to_string(), serde_json::Value::String("matrix test".to_string()));
                 inner_map.insert("N".to_string(), serde_json::Value::String(integer_number.to_string()));
-                let v = json!(res_factors);
-                inner_map.insert("result".to_string(), v);
                 
                 map.insert("success".to_string(), Value::Bool(true));
                 map.insert("payload".to_string(), serde_json::Value::Object(inner_map));
                 warp::reply::json(&map)
             },
             None => {
-                let integer_number: i64 = 2688834647444046;
-                let res_factors = factors(integer_number);
+                let integer_number: i64 = 100;
+                matrix(integer_number);
 
                 let mut map = Map::new();
                 let mut inner_map = Map::new();
-                inner_map.insert("test".to_string(), serde_json::Value::String("cpu test".to_string()));
+                inner_map.insert("test".to_string(), serde_json::Value::String("matrix test".to_string()));
                 inner_map.insert("N".to_string(), serde_json::Value::String(integer_number.to_string()));
-                let v = json!(res_factors);
-                inner_map.insert("result".to_string(), v);
                 
                 map.insert("success".to_string(), Value::Bool(true));
                 map.insert("payload".to_string(), serde_json::Value::Object(inner_map));
